baf62cd62c5ebca6d787d9a571c7e6fd456495a6
allania7med11 <allania7med11@gmail.com>
2024-06-04 07:21:27 +0100

    add host option

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Run Ansible playbook
-ansible-playbook --inventory ansible/inventory/vm-setup-playbook/hosts ansible/vm_setup_local.yml
+ansible-playbook --inventory ansible/inventory/vm-setup-playbook/hosts ansible/vm_setup_local.yml --extra-vars "host_on=$1" --ask-become-pass
